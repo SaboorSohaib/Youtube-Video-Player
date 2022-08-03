63d9c98d61e1379a26f246c5231d394ce51e8217
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-08-03 07:06:56 +0430

    javascript code for API

--- javascript.js
+++ javascript.js
@@ -1,17 +1,62 @@
+const videoCardContainer = document.querySelector('.video-container');
 let api_key = "AIzaSyCbq5lzL-gmCdW_It1Cie2Vr1oNoMVPNII";
-let video_http = "https://www.googleapis.com/youtube/v3/videos";
+let video_http = "https://www.googleapis.com/youtube/v3/videos?";
+let channel_http =" https://www.googleapis.com/youtube/v3/channels?"
 
 fetch(video_http + new URLSearchParams (
     {
         key: api_key,
         part: 'snippet',
         chart:'mostPopular',
-        maxResult:1,
+        maxResult: 10,
         regionCode:'IN'
     }
 ))
 
 .then(res => res.json())
 .then(data => {
-    console.log(data);
+    //console.log(data);
+    data.items.forEach(item => {
+        getChannelIcon(item);
+    })
 })
+.catch(err => console.log(err));
+const getChannelIcon = (video_data) => {
+    fetch(channel_http + new URLSearchParams({
+        key: api_key,
+        part: 'snippet',
+        id: video_data.snippet.channelId
+    }))
+    .then(res => res.json())
+    .then(data => {
+        video_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
+        makeVideoCard(video_data);
+    })
+
+}
+
+const makeVideoCard = (data) => {
+    videoCardContainer.innerHTML += `
+    <div class="video" onclick="location.href = 'https://youtube.com/watch?v=${data.id}'">;
+            <img src="${data.snippet.thumbnails.high.url}" class="thumbnail" alt="">
+        </div>
+        <div class="content">
+            <img src="${data.channelThumbnail}" class="channel-icon" alt="">
+            <div class="info">
+                <h4 class="title">${data.snippet.title}</h4>
+                <p class="channel-name">${data.snippet.channelTitle}</p>
+            </div>
+        </div>
+  `;
+}
+
+//search bar
+const searchInput = document.querySelector('.search-bar');
+const searchbtn = document.querySelector('.search-btn');
+let searchLink ="https://www.youtube.com/results?search_query=";
+
+searchbtn.addEventListener('click') , () => {
+    if(searchInput.ariaValueMax.length){
+        location.href = searchLink + searchInput.Value; 
+    }
+}
